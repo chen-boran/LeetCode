a6d7e60a99559aa3dc6b8ea2a67b9e9b9adf539a
chen-boran <2366642046@qq.com>
2021-12-10 00:41:10 +0800

    2

--- Solution/src/SortColors_075/Solution.java
+++ Solution/src/SortColors_075/Solution.java
@@ -1,2 +1,21 @@
-package SortColors_075;public class Solution {
+package SortColors_075;
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int re = Integer.MAX_VALUE;
+        int count =0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length ; j++) {
+                if(nums[j] < re ) {
+                    re = nums[j];
+                    count = j;
+                }
+            }
+            nums[count]=nums[i];
+            nums[i] = re ;
+            re = Integer.MAX_VALUE;
+        }
+    }
+
 }
+//每次选出最小的数，进行冒泡排序
