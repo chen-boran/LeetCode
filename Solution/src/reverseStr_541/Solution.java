fe0f3bb631a3e030615dbcea7730d195771511b5
cbr <2366642046@qq.com>
2022-03-05 10:47:27 +0800

    2022.3.5.2

--- Solution/src/reverseStr_541/Solution.java
+++ Solution/src/reverseStr_541/Solution.java
@@ -6,6 +6,23 @@ package reverseStr_541;
  */
 class Solution {
     public String reverseStr(String s, int k) {
+        int n = s.length();
 
+
+        char[] chars = s.toCharArray();
+        for (int l = 0,r= l +k; l < n;r+=2*k, l += 2 * k) {
+            reverse(chars, l, Math.min(r, n) - 1);//注意此处为什么是-1（长度减一即是下标，之后交换要用到数组的下标）
+        }
+        return new String(chars);
+    }
+
+    public void reverse(char[] chars, int left, int right) {
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
