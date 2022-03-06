f9be745632c48d0b01a95e589ff5a70dd0d51333
cbr <2366642046@qq.com>
2022-03-06 12:17:34 +0800

    2022.3.6.1

---
+++ Solution/src/getIntersectionNode_02_07/Solution.java
@@ -0,0 +1,54 @@
+package getIntersectionNode_02_07;
+
+/**
+ * @author ASUS
+ * @ 2022/3/6- 10:53
+
+*/
+
+
+
+
+class ListNode {
+int val;
+ListNode next;
+ListNode(int x) {
+val = x;
+next = null;
+}
+}
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int La =0;
+        int Lb = 0;
+        ListNode tmp1=headA;
+        ListNode tmp2=headB;
+        while (tmp1.next!= null){
+
+            La++;
+            tmp1=tmp1.next;
+        }
+        while (tmp2.next!= null){
+            Lb++;
+            tmp2=tmp2.next;
+        }
+        int gap = La-Lb;
+        // 让curA和curB在同一起点上（末尾位置对齐）
+        while (gap-- > 0) {
+            tmp1 = tmp1.next;
+        }
+
+        // 遍历curA 和 curB，遇到相同则直接返回
+        while (tmp1 != null) {
+            if (tmp1 == tmp2) {
+                return tmp1;
+            }
+            tmp1 = tmp1.next;
+            tmp2 = tmp2.next;
+        }
+        return null;
+    }
+
+
+}
